8762a3504220693b173ac51b9602d0269d4b8717
Setha Thay <sethathay@gmail.com>
2018-07-24 19:53:26 +0700

    backup files

---
+++ db/tbl_teacher_201807241946.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.tbl_teacher (id,active,address,caddress,dob,email,fstatus,gender,"name",phone,pob) VALUES 
+(1,true,'Phnom Penh','Phnom Penh','1989-03-01','sethathay@gmail.com',false,true,'Setha Thay','017330660','Kandal')
+,(2,true,'Phnom Penh','Phnom Penh','1990-05-27','sok.pisey@gmail.com',false,false,'Sok Pisey','098102987','Phnom Penh')
+;
