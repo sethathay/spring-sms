6a123b1c5fb71701f19761a13464a8dbb17adee4
Liling Meng <lilingmeng@Lilings-MacBook-Pro.local>
2018-07-27 00:31:57 +0700

    Add & Edit

--- spring-sms/src/main/java/com/tax/springsms/controllers/MainController.java
+++ spring-sms/src/main/java/com/tax/springsms/controllers/MainController.java
@@ -1,12 +1,25 @@
 package com.tax.springsms.controllers;
 
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tax.springsms.models.Teacher;
 import com.tax.springsms.services.TeacherService;
 
 @Controller
@@ -33,9 +46,18 @@ public class MainController {
 		req.setAttribute("teachers", tService.findAll());
 		return "teachers";
 	}
-	@GetMapping("/newTeacher")
-	public String addTeacher() {
-		return "newteacher";
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(Date.class,null,new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"),true));
+	}
+	
+	@PostMapping("/save")
+	public void saveTeacher(@ModelAttribute Teacher teacher,BindingResult bindingResult,HttpServletRequest req,HttpServletResponse resp) throws IOException {
+		tService.save(teacher);
+		req.setAttribute("teachers", tService.findAll());
+		req.setAttribute("mode", "VIEW");
+		resp.sendRedirect("/teachers");
 	}
 	@GetMapping("/updateTeacher")
 	public String updateTeacher(@RequestParam long id,HttpServletRequest req) {
@@ -54,6 +76,11 @@ public class MainController {
 	public String deleteTeacher(@RequestParam long id,HttpServletRequest req) {
 		tService.delete(id);
 		req.setAttribute("teachers", tService.findAll());
+		return "teachers";
+	}
+	@GetMapping("/newteacher")
+	public String addTeacher(HttpServletRequest req) {
+		req.setAttribute("mode", "NEW");
 		return "teachers";
 	}
 
